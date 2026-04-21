17571f5f958ef828254b426d0a8cbc3e811c847f
Ishaque Junejo <ishaque.junejo.dev+git@gmail.com>
2026-04-22 00:24:34 +0500

    Had mistakenly commented the testcases in src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,41 +6,41 @@
 #include "Circuit.h"
 
 int main () {
-    // srand(time(0));
+    srand(time(0));
 
 
-    // QubitRegister QR(3);
+    QubitRegister QR(3);
 
-    // std::cout << "Initial State\n";
-    // std::cout << QR << "\n";
+    std::cout << "Initial State\n";
+    std::cout << QR << "\n";
 
-    // QR.randomize();
+    QR.randomize();
 
-    // std::cout << "Randomized State\n";
-    // std::cout << QR << "\n";
+    std::cout << "Randomized State\n";
+    std::cout << QR << "\n";
 
-    // double probabilityOf1 = QR.probabilityOf(1, true);
-    // double probabilityOfNot1 = QR.probabilityOf(1, false);
+    double probabilityOf1 = QR.probabilityOf(1, true);
+    double probabilityOfNot1 = QR.probabilityOf(1, false);
 
-    // std::cout << "Probability of Qubit 1 being \'1\':\t" << probabilityOf1 << "\n";
-    // std::cout << "Probability of Qubit 1 being \'0\':\t" << probabilityOfNot1 << "\n";
+    std::cout << "Probability of Qubit 1 being \'1\':\t" << probabilityOf1 << "\n";
+    std::cout << "Probability of Qubit 1 being \'0\':\t" << probabilityOfNot1 << "\n";
 
-    // std::cout << "Total:\t" << probabilityOf1 + probabilityOfNot1 << "\n\n";
+    std::cout << "Total:\t" << probabilityOf1 + probabilityOfNot1 << "\n\n";
 
-    // QR.measure(1);
+    QR.measure(1);
 
-    // std::cout << "After measuring qubit index 1\n";
-    // std::cout << QR << "\n";
+    std::cout << "After measuring qubit index 1\n";
+    std::cout << QR << "\n";
 
-    // QR.measure(2);
+    QR.measure(2);
 
-    // std::cout << "After measuring qubit index 2\n";
-    // std::cout << QR << "\n";
+    std::cout << "After measuring qubit index 2\n";
+    std::cout << QR << "\n";
 
-    // QR.measure(0);
+    QR.measure(0);
 
-    // std::cout << "After measuring qubit index 0\n";
-    // std::cout << QR << "\n";
+    std::cout << "After measuring qubit index 0\n";
+    std::cout << QR << "\n";
     
     return 0;
 }
